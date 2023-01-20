6d3a7587bd6476ad2c40fb1ff8b235ab242e359b
alari777 <alari777@gmail.com>
2023-01-20 11:22:35 +0200

    added CreateStationsDTO

--- src/pages/api/v1/download/stations.ts
+++ src/pages/api/v1/download/stations.ts
@@ -5,13 +5,14 @@ import {
   Post,
   ValidationPipe,
 } from "next-api-decorators";
+import { CreateStationsDTO } from "@/pages/api/v1/download/dto/createStations.dto";
 
 // This class is for download csv file by remote URL
 class DownloadStations {
   // POST /v1/download/station
   @Post()
   @HttpCode(201)
-  async createStations(@Body(ValidationPipe) body: any) {
+  async createStations(@Body(ValidationPipe) body: CreateStationsDTO) {
     return 12;
   }
 }
