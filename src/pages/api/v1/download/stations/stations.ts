1476cb0de1167ad2bc35f2dfe89ce0c5f35a0e00
alari777 <alari777@gmail.com>
2023-01-20 19:15:41 +0200

    removed `StationType` and .map() by result of downloadCsv()

--- src/pages/api/v1/download/stations/stations.ts
+++ src/pages/api/v1/download/stations/stations.ts
@@ -8,22 +8,8 @@ import {
 } from "next-api-decorators";
 import { CreateStationsDTO } from "@/pages/api/v1/download/stations/dto/createStations.dto";
 import { downloadCsv } from "@/pages/api/v1/download/stations/helpers/dowloadCsv";
+import { prepareData } from "@/pages/api/v1/download/stations/helpers/prepareData";
 const prisma = new PrismaClient();
-
-type StationType = {
-  id: number;
-  nameFi: string;
-  nameSwe: string;
-  nameEn: string;
-  addressFi: string;
-  addressSwe: string;
-  cityFi: string;
-  citySwe: string;
-  operator: string;
-  capacities: number;
-  coordinateX: number;
-  coordinateY: number;
-};
 
 // This class is for download csv file by remote URL
 class DownloadStations {
@@ -32,29 +18,9 @@ class DownloadStations {
   @HttpCode(201)
   async createStations(@Body(ValidationPipe) body: CreateStationsDTO) {
     const stations: { data: string[] } = await downloadCsv(body);
-    const prepareArr: StationType[] = [];
-    stations.data.map(async (station: any) => {
-      if (!isNaN(Number(station[1]))) {
-        prepareArr.push({
-          id: Number(station[1]),
-          nameFi: station[2],
-          nameSwe: station[3],
-          nameEn: station[4],
-          addressFi: station[5],
-          addressSwe: station[6],
-          cityFi: station[7],
-          citySwe: station[8],
-          operator: station[9],
-          capacities: Number(station[10]),
-          coordinateX: parseFloat(station[11]),
-          coordinateY: parseFloat(station[12]),
-        });
-      } else {
-        console.log(station);
-      }
-    });
+    const data = prepareData(stations);
     const result = await prisma.stations.createMany({
-      data: prepareArr,
+      data,
     });
     await prisma.$disconnect();
     return result;
