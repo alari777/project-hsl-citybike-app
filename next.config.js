94480bc8cf93b02b1eee45dc820fd3ad0984f046
alari777 <alari777@gmail.com>
2023-01-20 10:22:05 +0200

    Preparing environment (#1)
    * updated .gitignore
    
    * was .nvmrc for nvm
    
    * installed prettier
    
    * added settings for prettier
    
    * added a setting in next.config for large data

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    largePageDataBytes: 128 * 1000,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
