f4c12d70d4c37a9e36c309af206057207b0f53be
alari777 <alari777@gmail.com>
2023-01-20 11:00:21 +0200

    created function donwloadStations(), method POST

--- src/pages/api/v1/download/stations.ts
+++ src/pages/api/v1/download/stations.ts
@@ -1,6 +1,14 @@
-import { createHandler } from "next-api-decorators";
+import {Body, createHandler, HttpCode, Post, ValidationPipe} from "next-api-decorators";
 
 // This class is for download csv file by remote URL
-class DownloadStations {}
+class DownloadStations {
+
+    // POST /v1/download/station
+    // This function downloads remote csv file with stations and adds data in DB
+    @Post()
+    @HttpCode(201)
+    async downloadStations(@Body(ValidationPipe) body: any) {
+        return 12;
+    }
 
 export default createHandler(DownloadStations);
