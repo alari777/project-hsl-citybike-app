96ed631ce408cfd338a9e13d8549e8cbb66d0bd9
alari777 <alari777@gmail.com>
2023-01-20 12:47:05 +0200

    Backend download csv (#2)
    * added folder for download routes
    
    * added stations route
    
    * removed stub file .gitkeep
    
    * installed next-api-decorators
    
    * init `DownloadStations` class
    
    * created function donwloadStations(), method POST
    
    * updated tsconfig
    
    * fixed `DownloadStations` class
    
    * renamed function `stations` to `createStations`
    
    * added CreateStationsDTO
    
    * installed class-validator
    
    * installed class-transformer
    
    * created CreateStationsDTO
    
    * added `helpers` folder for `/v1/download/stations`
    
    * installed papaparse
    
    * installed types for papaparse
    
    * added function `downloadCsv` for stations by remote URL
    
    * removed stub .gitkeep from `/v1/download/helpers/
    
    * renamed `DownloadCsv` to `downloadCsv` and refactored a little bit
    
    * called downloadCsv() in createdStations()
    
    * updated createStations()

---
+++ src/pages/api/v1/download/helpers/dowloadCsv.ts
@@ -0,0 +1,35 @@
+import * as Papa from "papaparse";
+import { InternalServerErrorException } from "next-api-decorators";
+
+type DownloadCsvType = {
+  data: string[];
+};
+
+type BodyType = {
+  url: string;
+};
+
+// This function downloads remote CSV file
+export async function downloadCsv(body: BodyType): Promise<DownloadCsvType> {
+  try {
+    const { url } = body;
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "content-type": "text/csv;charset=UTF-8",
+      },
+    });
+    if (response.status === 200) {
+      const res = await response.text();
+      // It is parsing and returning object response using `papaparse` package
+      return Papa.parse(res);
+    } else {
+      return {
+        data: [],
+      };
+    }
+  } catch (err: any) {
+    console.error(err.message);
+    throw new InternalServerErrorException("Something goes wrong ..");
+  }
+}
---
+++ src/pages/api/v1/download/stations.ts
@@ -0,0 +1,25 @@
+import {
+  Body,
+  createHandler,
+  HttpCode,
+  Post,
+  ValidationPipe,
+} from "next-api-decorators";
+import { CreateStationsDTO } from "@/pages/api/v1/download/dto/createStations.dto";
+import { downloadCsv } from "@/pages/api/v1/download/helpers/dowloadCsv";
+
+// This class is for download csv file by remote URL
+class DownloadStations {
+  // POST /v1/download/station
+  @Post()
+  @HttpCode(201)
+  async createStations(@Body(ValidationPipe) body: CreateStationsDTO) {
+    const stations: { data: string[] } = await downloadCsv(body);
+    // Will be refactored after adding `prisma orm`
+    console.log(stations.data.length);
+    stations.data.map(async (station: any, index: number) => {});
+    return 12;
+  }
+}
+
+export default createHandler(DownloadStations);
