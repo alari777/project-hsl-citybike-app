b05a1920e6f8f58bdcaf15cf4748886fb89418e0
alari777 <alari777@gmail.com>
2023-01-20 19:10:37 +0200

    moved `StationsType`

---
+++ src/pages/api/v1/download/stations/types/download.stations.types.ts
@@ -0,0 +1,14 @@
+export type StationType = {
+  id: number;
+  nameFi: string;
+  nameSwe: string;
+  nameEn: string;
+  addressFi: string;
+  addressSwe: string;
+  cityFi: string;
+  citySwe: string;
+  operator: string;
+  capacities: number;
+  coordinateX: number;
+  coordinateY: number;
+};
