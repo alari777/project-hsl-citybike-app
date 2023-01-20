b9d01880d0990f4e897b60756a16bc1a1262c548
alari777 <alari777@gmail.com>
2023-01-20 10:52:30 +0200

    init `DownloadStations` class

---
+++ src/pages/api/v1/download/stations.ts
@@ -0,0 +1,6 @@
+import { createHandler } from "next-api-decorators";
+
+// This class is for download csv file by remote URL
+class DownloadStations {}
+
+export default createHandler(DownloadStations);
