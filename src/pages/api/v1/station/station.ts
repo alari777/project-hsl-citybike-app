37b55bb0a72ab401d5824f2c70b830ddda1c2323
alari777 <alari777@gmail.com>
2023-01-21 17:35:31 +0200

    added DELETE route

--- src/pages/api/v1/station/station.ts
+++ src/pages/api/v1/station/station.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   createHandler,
+  Delete,
   Get,
   HttpCode,
   Post,
@@ -17,6 +18,11 @@ class Station {
   @Post()
   @HttpCode(201)
   async createStation(@Body(ValidationPipe) body: any) {}
+
+  // DELETE /api/v1/station/station
+  @Delete()
+  @HttpCode(201)
+  async deleteStation(@Body(ValidationPipe) body: any) {}
 }
 
 export default createHandler(Station);
