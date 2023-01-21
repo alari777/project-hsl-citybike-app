6d0fd8b744efff31cda88411bbdba1a065160842
alari777 <alari777@gmail.com>
2023-01-21 09:30:32 +0200

    added function body `downloadTripsCsv`

--- src/pages/api/v1/download/trips/helpers/downloadTripsCsv.ts
+++ src/pages/api/v1/download/trips/helpers/downloadTripsCsv.ts
@@ -1,1 +1,13 @@
-export async function downloadCsv(body: any): Promise<any> {}
+// This function downloads remote CSV file with trips
+export async function downloadTripsCsv(url: string): Promise<string> {
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "content-type": "text/csv;charset=UTF-8",
+    },
+  });
+  if (response.status === 200) {
+    return await response.text();
+  }
+  return "";
+}
