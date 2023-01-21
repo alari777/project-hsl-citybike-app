e242b0c601075d36673669950d01dac843a69ebe
alari777 <alari777@gmail.com>
2023-01-21 17:34:26 +0200

    added POST route

--- src/pages/api/v1/station/station.ts
+++ src/pages/api/v1/station/station.ts
@@ -1,9 +1,22 @@
-import { createHandler, Get, Query } from 'next-api-decorators';
+import {
+  Body,
+  createHandler,
+  Get,
+  HttpCode,
+  Post,
+  Query,
+  ValidationPipe,
+} from 'next-api-decorators';
 
 class Station {
   // GET /api/v1/station/station
   @Get()
   async fetchStation(@Query('fid') fid: string) {}
+
+  // POST /api/v1/station/station
+  @Post()
+  @HttpCode(201)
+  async createStation(@Body(ValidationPipe) body: any) {}
 }
 
 export default createHandler(Station);
