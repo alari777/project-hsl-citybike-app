475d2e1f7d67e58b0d05fda0af23f29c531e7dc8
alari777 <alari777@gmail.com>
2023-01-21 19:08:29 +0200

    added POST route

--- src/pages/api/v1/trip/trip.ts
+++ src/pages/api/v1/trip/trip.ts
@@ -1,9 +1,22 @@
-import { createHandler, Get, Query } from 'next-api-decorators';
+import {
+  Body,
+  createHandler,
+  Get,
+  HttpCode,
+  Post,
+  Query,
+  ValidationPipe,
+} from 'next-api-decorators';
 
 class Trip {
   // GET /api/v1/trip/trip
   @Get()
   async fetchTrip(@Query('id') id: string) {}
+
+  // POST /api/v1/trip/trip
+  @Post()
+  @HttpCode(201)
+  async createTrip(@Body(ValidationPipe) body: any) {}
 }
 
 export default createHandler(Trip);
