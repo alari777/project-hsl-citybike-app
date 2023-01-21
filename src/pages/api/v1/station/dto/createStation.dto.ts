e123c4cb1055486b8d5a8b5318c1116aaaabeb1c
alari777 <alari777@gmail.com>
2023-01-21 17:56:37 +0200

    added values and decorators for `createStationDTO`

---
+++ src/pages/api/v1/station/dto/createStation.dto.ts
@@ -0,0 +1,61 @@
+import {
+  IsDecimal,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateStationDTO {
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
+  public id!: number;
+
+  @IsNotEmpty()
+  @IsString()
+  public nameFi!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  public nameSwe!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  public nameEn!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  public addressFi!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  public addressSwe!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  public cityFi!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  public citySwe!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  public operator!: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  public capacities!: number;
+
+  @IsNotEmpty()
+  @IsDecimal()
+  @Min(0)
+  public coordinateX!: number;
+
+  @IsNotEmpty()
+  @IsDecimal()
+  @Min(0)
+  public coordinateY!: number;
+}
