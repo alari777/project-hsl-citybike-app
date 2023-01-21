48c34d59a50b3df5fac516b484cc61e9c9ac2e6e
alari777 <alari777@gmail.com>
2023-01-21 11:34:34 +0200

    createTripsCsvFile: prepare data in order to write these ones in file

--- src/pages/api/v1/download/trips/helpers/createTripsCsvFile.ts
+++ src/pages/api/v1/download/trips/helpers/createTripsCsvFile.ts
@@ -2,5 +2,33 @@ import * as Papa from "papaparse";
 
 export async function createTripsCsvFile(data: any): Promise<void> {
   const results: any = Papa.parse(data);
+
+  // To get IDs of stations in order to check each trip
   const stationsIds = await getIdsStations();
+
+  const trips: any = [];
+  results.data.filter((trip: any, index: number) => {
+    if (
+      // There are guards in order to prepare valid data for `Stations table`
+      index !== 0 &&
+      // Duration > 10.0s.
+      // !!!I found that at least one record is `DECIMAL`!!!
+      parseFloat(trip[7]) > 10 &&
+      // Covered Distance > 10.0m. !!!I found that at least one record is `DECIMAL`!!!
+      // !!!I found that at least one record is `DECIMAL`!!!
+      parseFloat(trip[6]) > 10 &&
+      stationsIds.includes(Number(trip[2])) && // Departure ID station is existing
+      stationsIds.includes(Number(trip[4])) // Return ID station is existing
+    ) {
+      trips.push({
+        departureDate: trip[0],
+        returnDate: trip[1],
+        departureStationId: Number(trip[2]),
+        returnStationId: Number(trip[4]),
+        coveredDistance: parseFloat(trip[6]),
+        duration: parseFloat(trip[7]),
+      });
+      return trip;
+    }
+  });
 }
