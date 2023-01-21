0bf53a0d81ed1cf0fec6d9f7518b18d99879b24e
alari777 <alari777@gmail.com>
2023-01-21 12:37:15 +0200

    added function body `getIdsStations`

--- src/pages/api/v1/download/trips/helpers/getIdsStations.ts
+++ src/pages/api/v1/download/trips/helpers/getIdsStations.ts
@@ -1,1 +1,18 @@
-export async function getIdsStations(data: any): Promise<void> {}
+import { PrismaClient } from "@prisma/client";
+
+type StationId = {
+  id: number;
+};
+
+export async function getIdsStations(): Promise<number[]> {
+  const prisma = new PrismaClient();
+  const stations: StationId[] = await prisma.stations.findMany({
+    select: { id: true },
+  });
+
+  await prisma.$disconnect();
+
+  return stations.map((station) => {
+    return station.id;
+  });
+}
