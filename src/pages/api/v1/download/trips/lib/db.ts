3f1f38e4e866713881aa9856e387087cc076f91d
alari777 <alari777@gmail.com>
2023-01-21 12:57:08 +0200

    fixed PORT value to db

--- src/pages/api/v1/download/trips/lib/db.ts
+++ src/pages/api/v1/download/trips/lib/db.ts
@@ -2,7 +2,7 @@ import mysql from "serverless-mysql";
 const db = mysql({
   config: {
     host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
+    port: Number(process.env.MYSQL_PORT),
     database: process.env.MYSQL_DATABASE,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
