284b0fcec328b32bdcacd96201682238c52c2950
alari777 <alari777@gmail.com>
2023-01-22 17:07:32 +0200

    render functional in order to fetch stations remotely

--- src/components/Manage/AddStationsRemotely/AddStationsRemotely.tsx
+++ src/components/Manage/AddStationsRemotely/AddStationsRemotely.tsx
@@ -1,7 +1,20 @@
 import { FC } from 'react';
 
 const AddStationsRemotely: FC = () => {
-  return <h1>AddStationsRemotely component</h1>;
+  return (
+    <>
+      <div className='form-group'>
+        <label htmlFor='stationsRemotelyUrl'>URL for fetching stations</label>
+        <input
+          type='url'
+          className='form-control'
+          id='stationsRemotelyUrl'
+          placeholder='Enter URL'
+        />
+      </div>
+      <button className='btn btn-primary mt-3'>Import stations</button>
+    </>
+  );
 };
 
 export default AddStationsRemotely;
