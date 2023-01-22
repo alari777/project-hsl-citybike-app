8acc7f5d32e9556b5375cfa1ed6b933ac1f02270
alari777 <alari777@gmail.com>
2023-01-22 22:43:13 +0200

    init AddStationsManually component

---
+++ src/components/Manage/AddStationsManually/AddStationsManually.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const AddStationsManually: FC = () => {
+  return (
+    <>
+      <h1>Add Stations Manually</h1>
+    </>
+  );
+};
+
+export default AddStationsManually;
