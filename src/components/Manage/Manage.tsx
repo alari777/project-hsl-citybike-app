35d0900a56bc8dda808c92c4944aea9d38fba48e
alari777 <alari777@gmail.com>
2023-01-22 17:08:08 +0200

    updated Manage component

--- src/components/Manage/Manage.tsx
+++ src/components/Manage/Manage.tsx
@@ -1,7 +1,13 @@
 import { FC } from 'react';
+import AddStationsRemotely from '@/components/Manage/AddStationsRemotely/AddStationsRemotely';
 
 const Manage: FC = () => {
-  return <h1>Manage component</h1>;
+  return (
+    <>
+      <h1>Manage component</h1>
+      <AddStationsRemotely />
+    </>
+  );
 };
 
 export default Manage;
