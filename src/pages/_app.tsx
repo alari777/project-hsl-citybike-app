892daa0a9d37377faa1951e99dea1e6a46871441
alari777 <alari777@gmail.com>
2023-01-22 09:08:02 +0200

    fixed: refactored + added few imports in `MyApp`, `_app.tsx`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Layout from '@/components/Layout/Layout';
+import '@/styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
