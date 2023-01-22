83b4b3af3c4d5170c93ef772486f9425097f80da
alari777 <alari777@gmail.com>
2023-01-22 09:03:40 +0200

    refactored `MyApp` in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Layout>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </Layout>
+  );
+};
+
+export default MyApp;
