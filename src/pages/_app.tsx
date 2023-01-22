aaf23d57c62fee05aa812a0ba94b2e25ba69c5d3
alari777 <alari777@gmail.com>
2023-01-22 09:16:52 +0200

    fixed `_app.tsx`, valid import path for `global.scss`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout/Layout';
-import '@/styles/globals.css';
+import '@/styles/globals.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
