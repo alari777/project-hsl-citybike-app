0d7f862a16358f9272cdc7d49467188f860e3210
alari777 <alari777@gmail.com>
2023-01-22 19:25:31 +0200

    applied attribute `typeReport` in component `Report`

--- src/components/Manage/Report/Report.tsx
+++ src/components/Manage/Report/Report.tsx
@@ -2,12 +2,13 @@ import { FC } from 'react';
 
 interface ReportProps {
   reports: string[];
+  typeReport: string;
 }
 
-const Report: FC<ReportProps> = ({ reports }) => {
+const Report: FC<ReportProps> = ({ reports, typeReport }) => {
   return (
     <>
-      <ul>
+      <ul id={typeReport} key={typeReport}>
         {reports &&
           reports.map((report: string, index: number) => (
             <li key={index}>{report}</li>
