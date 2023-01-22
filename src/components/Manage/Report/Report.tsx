06f4bc8e0bf8ee7e9380751d99310104b10abb04
alari777 <alari777@gmail.com>
2023-01-22 21:34:46 +0200

    added label for report and margin-top

--- src/components/Manage/Report/Report.tsx
+++ src/components/Manage/Report/Report.tsx
@@ -7,14 +7,15 @@ interface ReportProps {
 
 const Report: FC<ReportProps> = ({ reports, typeReport }) => {
   return (
-    <>
+    <div className='mt-3'>
+      <label htmlFor={typeReport}>Report:</label>
       <ul id={typeReport} key={typeReport}>
         {reports &&
           reports.map((report: string, index: number) => (
             <li key={index}>{report}</li>
           ))}
       </ul>
-    </>
+    </div>
   );
 };
 
