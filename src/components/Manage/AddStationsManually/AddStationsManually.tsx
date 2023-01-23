856afd7010b50dd4f9b42153bc28b09fa8e0e1b0
alari777 <alari777@gmail.com>
2023-01-23 11:14:42 +0200

    were added `onChange` for all inputs: add new station manually

--- src/components/Manage/AddStationsManually/AddStationsManually.tsx
+++ src/components/Manage/AddStationsManually/AddStationsManually.tsx
@@ -36,10 +36,16 @@ const AddStationsManually: FC = () => {
         <div className='form-group'>
           <label htmlFor='idStation'>ID station</label>
           <input
-            type='text'
+            type='number'
             className='form-control'
             id='idStation'
             placeholder='Enter ID station'
+            onChange={(e) =>
+              setOneStation({
+                ...oneStation,
+                id: Number(e.target.value),
+              })
+            }
           />
 
           <label htmlFor='nameFi'>Station name FI</label>
@@ -48,6 +54,12 @@ const AddStationsManually: FC = () => {
             className='form-control'
             id='nameFi'
             placeholder='Enter station name FI'
+            onChange={(e) =>
+              setOneStation({
+                ...oneStation,
+                nameFi: e.target.value,
+              })
+            }
           />
           <label htmlFor='nameSwe'>Station name SWE</label>
           <input
@@ -55,6 +67,12 @@ const AddStationsManually: FC = () => {
             className='form-control'
             id='nameSwe'
             placeholder='Enter station name SWE'
+            onChange={(e) =>
+              setOneStation({
+                ...oneStation,
+                nameSwe: e.target.value,
+              })
+            }
           />
           <label htmlFor='nameEn'>Station name EN</label>
           <input
@@ -62,6 +80,12 @@ const AddStationsManually: FC = () => {
             className='form-control'
             id='nameEn'
             placeholder='Enter station name EN'
+            onChange={(e) =>
+              setOneStation({
+                ...oneStation,
+                nameEn: e.target.value,
+              })
+            }
           />
           <label htmlFor='addressFi'>Station address FI</label>
           <input
@@ -69,6 +93,12 @@ const AddStationsManually: FC = () => {
             className='form-control'
             id='addressFi'
             placeholder='Enter address FI'
+            onChange={(e) =>
+              setOneStation({
+                ...oneStation,
+                addressFi: e.target.value,
+              })
+            }
           />
           <label htmlFor='addressSwe'>Station address SWE</label>
           <input
@@ -76,6 +106,12 @@ const AddStationsManually: FC = () => {
             className='form-control'
             id='addressSwe'
             placeholder='Enter address SWE'
+            onChange={(e) =>
+              setOneStation({
+                ...oneStation,
+                addressSwe: e.target.value,
+              })
+            }
           />
           <label htmlFor='cityFi'>Enter city FI</label>
           <input
@@ -83,6 +119,12 @@ const AddStationsManually: FC = () => {
             className='form-control'
             id='cityFi'
             placeholder='Enter city FI'
+            onChange={(e) =>
+              setOneStation({
+                ...oneStation,
+                cityFi: e.target.value,
+              })
+            }
           />
           <label htmlFor='citySwe'>Enter city SWE</label>
           <input
@@ -90,6 +132,12 @@ const AddStationsManually: FC = () => {
             className='form-control'
             id='citySwe'
             placeholder='Enter city SWE'
+            onChange={(e) =>
+              setOneStation({
+                ...oneStation,
+                citySwe: e.target.value,
+              })
+            }
           />
           <label htmlFor='operator'>Enter operator</label>
           <input
@@ -97,6 +145,12 @@ const AddStationsManually: FC = () => {
             className='form-control'
             id='operator'
             placeholder='Enter operator'
+            onChange={(e) =>
+              setOneStation({
+                ...oneStation,
+                operator: e.target.value,
+              })
+            }
           />
           <label htmlFor='capacities'>Enter capacities</label>
           <input
@@ -104,6 +158,12 @@ const AddStationsManually: FC = () => {
             className='form-control'
             id='capacities'
             placeholder='Enter capacities'
+            onChange={(e) =>
+              setOneStation({
+                ...oneStation,
+                capacities: Number(e.target.value),
+              })
+            }
           />
           <label htmlFor='coordinateX'>Enter coordinateX</label>
           <input
@@ -111,6 +171,12 @@ const AddStationsManually: FC = () => {
             className='form-control'
             id='coordinateX'
             placeholder='Enter coordinateX'
+            onChange={(e) =>
+              setOneStation({
+                ...oneStation,
+                coordinateX: Number(e.target.value),
+              })
+            }
           />
           <label htmlFor='coordinateY'>Enter coordinateY</label>
           <input
@@ -118,6 +184,12 @@ const AddStationsManually: FC = () => {
             className='form-control'
             id='coordinateY'
             placeholder='Enter coordinateY'
+            onChange={(e) =>
+              setOneStation({
+                ...oneStation,
+                coordinateY: Number(e.target.value),
+              })
+            }
           />
         </div>
         <button type='submit' className='btn btn-primary mt-3'>
