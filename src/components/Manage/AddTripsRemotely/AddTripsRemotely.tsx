6b66cfb6f21ff3ac1ef779a3672c5cb7759ab476
alari777 <alari777@gmail.com>
2023-01-23 12:52:41 +0200

    init `importTrips()` function in order to fetch trips remotely

--- src/components/Manage/AddTripsRemotely/AddTripsRemotely.tsx
+++ src/components/Manage/AddTripsRemotely/AddTripsRemotely.tsx
@@ -1,10 +1,46 @@
-import { FC, useState } from 'react';
+import { FC, FormEvent, useState } from 'react';
 import Report from '@/components/Manage/Report/Report';
 
 const AddTripsRemotely: FC = () => {
   const [urlTrip, setUrlTrip] = useState<string>('');
   const [classSpinner, setClassSpinner] = useState<boolean>(false);
   const [report, setReport] = useState<string[]>([]);
+
+  // Add remotely trips in DB
+  const importTrips = async (
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event.preventDefault();
+    setClassSpinner(true);
+    setReport([]);
+    setReport((report) => [
+      ...report,
+      'Wait a little bit. Data is fetching remotely ...',
+    ]);
+    try {
+      const response = await fetch('/api/v1/download/trips/trips', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          url: urlTrip,
+        }),
+      });
+      if (response.status === 201) {
+        setReport((report) => [...report, 'Adding trips in DB.']);
+        await response.json();
+        setReport((report) => [...report, 'Trips were added successfully.']);
+      }
+    } catch (err) {
+      setReport((report) => [
+        ...report,
+        'Data was not added. Something went wrong. Please try again later.',
+      ]);
+    }
+    setClassSpinner(false);
+  };
 
   return (
     <div>
