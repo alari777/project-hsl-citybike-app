5e7293878d7fc534d6f7a7dfe4a7a23f9da57c31
alari777 <alari777@gmail.com>
2023-01-23 12:46:10 +0200

    added hook useState for store remotely trips URL

--- src/components/Manage/AddTripsRemotely/AddTripsRemotely.tsx
+++ src/components/Manage/AddTripsRemotely/AddTripsRemotely.tsx
@@ -1,7 +1,8 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Report from '@/components/Manage/Report/Report';
 
 const AddTripsRemotely: FC = () => {
+  const [urlTrip, setUrlTrip] = useState<string>('');
   return (
     <>
       <h1>Add Trips</h1>
