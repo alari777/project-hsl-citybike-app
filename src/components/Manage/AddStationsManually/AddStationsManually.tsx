04fcb8cecd2b4879d1a2e02ebb9cf5fa1834048c
alari777 <alari777@gmail.com>
2023-01-23 09:32:12 +0200

    added input tag `idStation`: add new station manually

--- src/components/Manage/AddStationsManually/AddStationsManually.tsx
+++ src/components/Manage/AddStationsManually/AddStationsManually.tsx
@@ -4,6 +4,15 @@ const AddStationsManually: FC = () => {
   return (
     <>
       <form>
+        <div className='form-group'>
+          <label htmlFor='idStation'>ID station</label>
+          <input
+            type='text'
+            className='form-control'
+            id='idStation'
+            placeholder='Enter ID station'
+          />
+        </div>
         <button type='submit' className='btn btn-primary mt-3'>
           Add station
         </button>
