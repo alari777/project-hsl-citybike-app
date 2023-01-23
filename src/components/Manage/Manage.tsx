f8663432f98331a8b7784895c320cfa89bbd4d3b
alari777 <alari777@gmail.com>
2023-01-23 09:52:52 +0200

    render AddStationsManually component on page Manage

--- src/components/Manage/Manage.tsx
+++ src/components/Manage/Manage.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import AddStationsRemotely from '@/components/Manage/AddStationsRemotely/AddStationsRemotely';
+import AddStationsManually from '@/components/Manage/AddStationsManually/AddStationsManually';
 
 const Manage: FC = () => {
   return (
     <>
       <AddStationsRemotely />
+      <AddStationsManually />
     </>
   );
 };
