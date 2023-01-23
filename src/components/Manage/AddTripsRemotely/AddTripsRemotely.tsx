a02d9000c60c8c9cf7d20317eaf1845e353e2580
alari777 <alari777@gmail.com>
2023-01-23 12:50:15 +0200

    render form tag

--- src/components/Manage/AddTripsRemotely/AddTripsRemotely.tsx
+++ src/components/Manage/AddTripsRemotely/AddTripsRemotely.tsx
@@ -7,9 +7,36 @@ const AddTripsRemotely: FC = () => {
   const [report, setReport] = useState<string[]>([]);
 
   return (
-    <>
-      <h1>Add Trips</h1>
-    </>
+    <div>
+      <form>
+        <div className='form-group'>
+          <label htmlFor='tripsRemotelyUrl'>URL for fetching trips *</label>
+          <input
+            type='url'
+            className='form-control'
+            id='tripsRemotelyUrl'
+            placeholder='Enter URL'
+            required={true}
+          />
+        </div>
+        <button
+          type='submit'
+          id='submitTripsRemotely'
+          className='btn btn-primary mt-3'
+        >
+          {classSpinner && (
+            <span
+              className='spinner-border spinner-border-sm'
+              role='status'
+              aria-hidden='true'
+              id='spinnerTripsRemotely'
+            ></span>
+          )}
+          Import trips
+        </button>
+        <Report reports={report} typeReport='AddTripsRemotely' />
+      </form>
+    </div>
   );
 };
 
