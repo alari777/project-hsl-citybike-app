80f0c426775834e7380ec6f955d247a1101f6a35
alari777 <alari777@gmail.com>
2023-01-23 12:58:55 +0200

    render AddTripsRemotely component on page Manage

--- src/components/Manage/Manage.tsx
+++ src/components/Manage/Manage.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 import AddStationsRemotely from '@/components/Manage/AddStationsRemotely/AddStationsRemotely';
 import AddStationsManually from '@/components/Manage/AddStationsManually/AddStationsManually';
+import AddTripsRemotely from '@/components/Manage/AddTripsRemotely/AddTripsRemotely';
 
 const Manage: FC = () => {
   return (
     <>
       <AddStationsRemotely />
       <AddStationsManually />
+      <AddTripsRemotely />
     </>
   );
 };
