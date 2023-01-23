3eea6c02a4c91a7a790874c5f95b37aa12b6c6a1
alari777 <alari777@gmail.com>
2023-01-23 12:47:07 +0200

    added hook useState for manage reports messages`

--- src/components/Manage/AddTripsRemotely/AddTripsRemotely.tsx
+++ src/components/Manage/AddTripsRemotely/AddTripsRemotely.tsx
@@ -4,6 +4,8 @@ import Report from '@/components/Manage/Report/Report';
 const AddTripsRemotely: FC = () => {
   const [urlTrip, setUrlTrip] = useState<string>('');
   const [classSpinner, setClassSpinner] = useState<boolean>(false);
+  const [report, setReport] = useState<string[]>([]);
+
   return (
     <>
       <h1>Add Trips</h1>
