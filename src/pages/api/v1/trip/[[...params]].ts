220b16de88968c9ec74754553517f193580c2277
alari777 <alari777@gmail.com>
2023-01-24 09:58:01 +0200

    added new 2 passed Query params: `distance` and `duration`

--- src/pages/api/v1/trip/[[...params]].ts
+++ src/pages/api/v1/trip/[[...params]].ts
@@ -3,13 +3,18 @@ import {
   Get,
   Param,
   ParseNumberPipe,
+  Query,
 } from 'next-api-decorators';
 import { getTrips } from '@/pages/api/getTrips';
 
 class TripParams {
   @Get('/:page')
-  async getFewTrips(@Param('page', ParseNumberPipe) page: number) {
-    return await getTrips(page);
+  async getFewTrips(
+    @Param('page', ParseNumberPipe) page: number,
+    @Query('distance', ParseNumberPipe) distance: number,
+    @Query('duration', ParseNumberPipe) duration: number
+  ) {
+    return await getTrips(page, distance, duration);
   }
 }
 
