8724f2451468bad8157af6acbb7035a8a40263c7
alari777 <alari777@gmail.com>
2023-01-24 07:53:24 +0200

    called `findMany()` in order to get 100 trips-records from DB

--- src/pages/api/getTrips.ts
+++ src/pages/api/getTrips.ts
@@ -4,4 +4,20 @@ const prisma = new PrismaClient();
 
 export const getTrips = async () => {
   console.debug('getTrips');
+  const trips = await prisma.trips.findMany({
+    include: {
+      Stations_Trips_departureStationIdToStations: {
+        select: {
+          nameFi: true,
+        },
+      },
+      Stations_Trips_returnStationIdToStations: {
+        select: {
+          nameFi: true,
+        },
+      },
+    },
+    skip: 0,
+    take: 100,
+  });
 };
