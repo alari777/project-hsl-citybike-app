f3b6389ef2904422955a230c0facf3141aab2389
alari777 <alari777@gmail.com>
2023-01-24 07:58:04 +0200

    added comment to `findMany() query`

--- src/pages/api/getTrips.ts
+++ src/pages/api/getTrips.ts
@@ -4,13 +4,18 @@ const prisma = new PrismaClient();
 
 export const getTrips = async () => {
   console.debug('getTrips');
+  // To get first 100 trips-records from DB.
+  // Also response per each trip has 2 short joined object with related stations:
+  // `departure` and `return`
   const trips = await prisma.trips.findMany({
     include: {
+      // NameFi of departure station
       Stations_Trips_departureStationIdToStations: {
         select: {
           nameFi: true,
         },
       },
+      // NameFi of return station
       Stations_Trips_returnStationIdToStations: {
         select: {
           nameFi: true,
