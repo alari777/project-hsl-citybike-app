da414926672c562c3b2aa3f890d770f03c295c1a
alari777 <alari777@gmail.com>
2023-01-25 18:54:57 +0200

    render contant-table with single station

--- src/pages/stations/[slug].tsx
+++ src/pages/stations/[slug].tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { StationType } from '@/types/manage.types';
 import { GetServerSidePropsContext } from 'next';
+import { getSlugStation } from '@/pages/api/getSlugStation';
 
 interface SingleStationPageProps {
   stations: StationType[];
@@ -9,7 +10,37 @@ interface SingleStationPageProps {
 const SingleStationPage: FC<SingleStationPageProps> = ({ stations }) => {
   return (
     <>
-      <h1>Slug Station Page</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th scope='col'>FID</th>
+            <th scope='col'>Name FI</th>
+            <th scope='col'>Name SWE</th>
+            <th scope='col'>Name EN</th>
+            <th scope='col'>Address FI</th>
+            <th scope='col'>Address SWE</th>
+            <th scope='col'>Amount starting</th>
+            <th scope='col'>Amount ending</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stations &&
+            stations.map((station: StationType) => (
+              <tr key={station.nameFi}>
+                <td>{station.fid}</td>
+                <td>{station.nameFi}</td>
+                <td>{station.nameSwe}</td>
+                <td>{station.nameEn}</td>
+                <td>{station.addressFi}</td>
+                <td>{station.addressSwe}</td>
+                <td>
+                  {station._count.Trips_Trips_departureStationIdToStations}
+                </td>
+                <td>{station._count.Trips_Trips_returnStationIdToStations}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </>
   );
 };
