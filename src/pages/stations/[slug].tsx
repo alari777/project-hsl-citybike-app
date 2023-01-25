5ceb8c1a1a4d2104a7433af876b1b59daf361866
alari777 <alari777@gmail.com>
2023-01-25 18:48:44 +0200

    added getServerSideProps on the [slug] single station page

--- src/pages/stations/[slug].tsx
+++ src/pages/stations/[slug].tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { StationType } from '@/types/manage.types';
+import { GetServerSidePropsContext } from 'next';
 
 interface SingleStationPageProps {
   stations: StationType[];
@@ -14,3 +15,13 @@ const SingleStationPage: FC<SingleStationPageProps> = ({ stations }) => {
 };
 
 export default SingleStationPage;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const query = context.query;
+  const slug = query.slug as string;
+  const stations = await getSlugStation(slug);
+
+  return {
+    props: { stations: JSON.parse(stations) }, // will be passed to the page component as props
+  };
+}
