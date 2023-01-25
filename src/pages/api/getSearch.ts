b3a5633e5550a7170287cf5787dd41eb1c383039
alari777 <alari777@gmail.com>
2023-01-25 08:07:49 +0200

    added function body: `searchInTrips`

--- src/pages/api/getSearch.ts
+++ src/pages/api/getSearch.ts
@@ -3,6 +3,33 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 // This function makes search by stations in `Trips` table
-export const searchInTrips = async () => {
-  return '';
+export const searchInTrips = async (
+  pageNumber: number,
+  filterDistance: number = 10,
+  filterDuration: number = 10,
+  search = ''
+) => {
+  const skip = pageNumber * 100;
+  const take = 100;
+  const trips = await prisma.trips.findMany({
+    include: {
+      // NameFi of departure station
+      Stations_Trips_departureStationIdToStations: {
+        select: {
+          nameFi: true,
+        },
+      },
+      // NameFi of return station
+      Stations_Trips_returnStationIdToStations: {
+        select: {
+          nameFi: true,
+        },
+      },
+    },
+    skip: skip,
+    take: take,
+  });
+
+  await prisma.$disconnect();
+  return trips;
 };
