f5015579a9c716f16bbc6cc3cd7e76cfa5b638e5
alari777 <alari777@gmail.com>
2023-01-25 08:12:35 +0200

    called function `searchInTrips` in route `GET /search`

--- src/pages/api/v1/search/[[...params]].ts
+++ src/pages/api/v1/search/[[...params]].ts
@@ -5,16 +5,17 @@ import {
   ParseNumberPipe,
   Query,
 } from 'next-api-decorators';
+import { searchInTrips } from '@/pages/api/searchInTrips';
 
 class SearchParams {
   @Get('/:page')
   async searchTrips(
-    @Query('search') search: string,
     @Param('page', ParseNumberPipe) page: number,
     @Query('distance', ParseNumberPipe) distance: number,
-    @Query('duration', ParseNumberPipe) duration: number
+    @Query('duration', ParseNumberPipe) duration: number,
+    @Query('search') search: string
   ) {
-    return true;
+    return await searchInTrips(page, distance, duration, search);
   }
 }
 
