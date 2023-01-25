ece741a9b8c517df384aa8ba1b565e9b89ad1116
alari777 <alari777@gmail.com>
2023-01-25 08:02:13 +0200

    import prisma client

--- src/pages/api/getSearch.ts
+++ src/pages/api/getSearch.ts
@@ -1,3 +1,7 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 // This function makes search by stations in `Trips` table
 export const searchInTrips = async () => {
   return '';
