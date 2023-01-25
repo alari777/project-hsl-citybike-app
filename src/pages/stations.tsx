c133d25b94a487dcf6959b45ab11aeb4a7fd5e09
alari777 <alari777@gmail.com>
2023-01-25 18:35:18 +0200

    rendered table with stations (without pagination so far)

--- src/pages/stations.tsx
+++ src/pages/stations.tsx
@@ -10,7 +10,41 @@ interface StationsPageProps {
 const StationsPage: FC<StationsPageProps> = ({ stations }) => {
   return (
     <>
-      <h1>Stations Page</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th scope='col'>FID</th>
+            <th scope='col'>Name FI</th>
+            <th scope='col'>Name SWE</th>
+            <th scope='col'>Name EN</th>
+            <th scope='col'>Address FI</th>
+            <th scope='col'>Address SWE</th>
+            <th scope='col'>City FI</th>
+            <th scope='col'>City SWE</th>
+            <th scope='col'>Capacities</th>
+            <th scope='col'>Coordinate X</th>
+            <th scope='col'>Coordinate Y</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stations &&
+            stations.map((station: StationType) => (
+              <tr key={station.nameFi}>
+                <td>{station.fid}</td>
+                <td>{station.nameFi}</td>
+                <td>{station.nameSwe}</td>
+                <td>{station.nameEn}</td>
+                <td>{station.addressFi}</td>
+                <td>{station.addressSwe}</td>
+                <td>{station.cityFi}</td>
+                <td>{station.citySwe}</td>
+                <td>{station.capacities}</td>
+                <td>{station.coordinateX}</td>
+                <td>{station.coordinateY}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </>
   );
 };
@@ -18,7 +52,7 @@ const StationsPage: FC<StationsPageProps> = ({ stations }) => {
 export default StationsPage;
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  // Getting station
+  // Getting stations
   const stations = await getStations();
   return {
     props: { stations: JSON.parse(stations) }, // will be passed to the page component as props
