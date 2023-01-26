0ed7f3c144399f02ff8800eb22ab2fbbc5f86f32
alari777 <alari777@gmail.com>
2023-01-26 09:26:06 +0200

    refactored function `getStations` on SSR side and added pagination there

--- src/pages/api/getStations.ts
+++ src/pages/api/getStations.ts
@@ -3,8 +3,13 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 // This function gets all available stations
-export const getStations = async () => {
-  const stations = await prisma.stations.findMany();
+export const getStations = async (pageNumber: number) => {
+  const skip = pageNumber * 100;
+  const take = 100;
+  const stations = await prisma.stations.findMany({
+    skip: skip,
+    take: take,
+  });
 
   await prisma.$disconnect();
   return JSON.stringify(stations);
