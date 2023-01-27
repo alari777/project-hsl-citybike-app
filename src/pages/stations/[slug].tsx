287d4ac91bf3debdd90f150f0a80eadfcc76c264
alari777 <alari777@gmail.com>
2023-01-27 09:40:10 +0200

    added `Top5Type` for props, new props variables, new ssr variables

--- src/pages/stations/[slug].tsx
+++ src/pages/stations/[slug].tsx
@@ -3,6 +3,14 @@ import { StationType } from '@/types/manage.types';
 import { GetServerSidePropsContext } from 'next';
 import { getSlugStation } from '@/pages/api/getSlugStation';
 
+type Top5Type = {
+  departureStationId: number;
+  name1: string;
+  returnStationId: number;
+  name2: string;
+  DepartureCount: number;
+};
+
 interface SingleStationPageProps {
   stations: StationType[];
   averageDepartureDistance: {
@@ -11,12 +19,16 @@ interface SingleStationPageProps {
   averageReturnDistance: {
     rCoveredDistance: number;
   }[];
+  top5Departure: Top5Type[];
+  top5Return: Top5Type[];
 }
 
 const SingleStationPage: FC<SingleStationPageProps> = ({
   stations,
   averageDepartureDistance,
   averageReturnDistance,
+  top5Departure,
+  top5Return,
 }) => {
   return (
     <>
@@ -89,6 +101,8 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
       stations: results[0],
       averageDepartureDistance: results[1],
       averageReturnDistance: results[2],
+      top5Departure: results[3],
+      top5Return: results[4],
     }, // will be passed to the page component as props
   };
 }
