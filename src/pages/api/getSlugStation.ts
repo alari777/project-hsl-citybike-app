5f36e05c1c00db05fa806d9e9530e0a547a32054
alari777 <alari777@gmail.com>
2023-01-27 09:50:19 +0200

    Frontend single station view addtional top5 (#21)
    * added: top 5 most popular return stations for journeys starting from the station
    
    * added: top 5 most popular departure stations for journeys ending at the station
    
    * refactored queries to get top5 stations. prisma dose not work well with hard query so refactoring
    
    * removed comments in orde to clear code
    
    * added `Top5Type` and refactored some pixels of code
    
    * added `Top5Type` for props, new props variables, new ssr variables
    
    * added: `top 5 most popular RETURN stations for journeys starting from the station`
    
    * added: `top 5 most popular DEPARTURE stations for journeys starting from the station`
    
    * fixed queries for `top 5 stations`
    
    * refactored view of `top 5 returned stations`

--- src/pages/api/getSlugStation.ts
+++ src/pages/api/getSlugStation.ts
@@ -1,6 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 const prisma = new PrismaClient();
+
+type Top5Type = {
+  departureStationId: number;
+  name1: string;
+  returnStationId: number;
+  name2: string;
+  DepartureCount: number;
+};
 
 // This function gets all available stations
 export const getSlugStation = async (fid: string): Promise<string> => {
@@ -20,16 +28,66 @@ export const getSlugStation = async (fid: string): Promise<string> => {
     },
   });
 
+  // For: the average distance of a journey starting from the station
   const totalDepartureDistance =
     await prisma.$queryRaw`SELECT SUM(d.coveredDistance) as dCoveredDistance FROM Trips AS d WHERE d.departureStationId = ${Number(
       fid
     )}`;
+
+  // For: the average distance of a journey ending at the station
   const totalReturnDistance =
     await prisma.$queryRaw`SELECT SUM(r.coveredDistance) as rCoveredDistance FROM Trips AS r WHERE r.returnStationId = ${Number(
       fid
     )}`;
 
+  // Top 5 most popular return stations for journeys starting from the station
+  const topReturnStations: Top5Type[] =
+    await prisma.$queryRaw`SELECT t.departureStationId, s2.nameFi as name1, t.returnStationId, s1.nameFi as name2, COUNT(t.departureStationId) AS DepartureCount FROM Trips AS t  LEFT JOIN Stations AS s1 ON s1.fid = ${Number(
+      fid
+    )}  LEFT JOIN Stations AS s2 ON s2.fid = t.departureStationId  WHERE t.returnStationId = ${Number(
+      fid
+    )} GROUP BY t.departureStationId ORDER BY DepartureCount DESC LIMIT 5`;
+
+  const top5Return: Top5Type[] = [];
+  topReturnStations.map((item) => {
+    const foo = {
+      departureStationId: item.departureStationId,
+      name1: item.name1,
+      returnStationId: item.returnStationId,
+      name2: item.name2,
+      DepartureCount: Number(item.DepartureCount),
+    };
+    top5Return.push(foo);
+  });
+
+  // Top 5 most popular departure stations for journeys ending at the station
+  const topDepartureStations: Top5Type[] = await prisma.$queryRaw(
+    Prisma.sql`SELECT t.departureStationId, s2.nameFi as name1, t.returnStationId, s1.nameFi as name2, COUNT(t.returnStationId) AS DepartureCount FROM Trips AS t LEFT JOIN Stations AS s1 ON s1.fid = ${Number(
+      fid
+    )} LEFT JOIN Stations AS s2 ON s2.fid = t.returnStationId WHERE t.departureStationId = ${Number(
+      fid
+    )} GROUP BY t.returnStationId ORDER BY DepartureCount DESC LIMIT 5`
+  );
+
+  const top5Departure: Top5Type[] = [];
+  topDepartureStations.map((item) => {
+    const foo = {
+      returnStationId: item.returnStationId,
+      name1: item.name1,
+      departureStationId: item.departureStationId,
+      name2: item.name2,
+      DepartureCount: Number(item.DepartureCount),
+    };
+    top5Departure.push(foo);
+  });
+
   await prisma.$disconnect();
 
-  return JSON.stringify([station, totalDepartureDistance, totalReturnDistance]);
+  return JSON.stringify([
+    station,
+    totalDepartureDistance,
+    totalReturnDistance,
+    top5Return,
+    top5Departure,
+  ]);
 };
--- src/pages/stations/[slug].tsx
+++ src/pages/stations/[slug].tsx
@@ -3,6 +3,14 @@ import { StationType } from '@/types/manage.types';
 import { GetServerSidePropsContext } from 'next';
 import { getSlugStation } from '@/pages/api/getSlugStation';
 
+type Top5Type = {
+  departureStationId: number;
+  name1: string;
+  returnStationId: number;
+  name2: string;
+  DepartureCount: number;
+};
+
 interface SingleStationPageProps {
   stations: StationType[];
   averageDepartureDistance: {
@@ -11,12 +19,16 @@ interface SingleStationPageProps {
   averageReturnDistance: {
     rCoveredDistance: number;
   }[];
+  top5Departure: Top5Type[];
+  top5Return: Top5Type[];
 }
 
 const SingleStationPage: FC<SingleStationPageProps> = ({
   stations,
   averageDepartureDistance,
   averageReturnDistance,
+  top5Departure,
+  top5Return,
 }) => {
   return (
     <>
@@ -71,6 +83,54 @@ const SingleStationPage: FC<SingleStationPageProps> = ({
             ))}
         </tbody>
       </table>
+
+      <table className='table'>
+        <thead>
+          <tr>
+            <th scope='col'>Departure ID</th>
+            <th scope='col'>Station Name</th>
+            <th scope='col'>Return ID</th>
+            <th scope='col'>Station name</th>
+            <th scope='col'>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {top5Return &&
+            top5Return.map((station: Top5Type) => (
+              <tr key={station.returnStationId}>
+                <td>{station.departureStationId}</td>
+                <td>{station.name2}</td>
+                <td>{station.returnStationId}</td>
+                <td>{station.name1}</td>
+                <td>{station.DepartureCount}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+
+      <table className='table'>
+        <thead>
+          <tr>
+            <th scope='col'>Departure ID</th>
+            <th scope='col'>Station Name</th>
+            <th scope='col'>Return ID</th>
+            <th scope='col'>Station name</th>
+            <th scope='col'>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {top5Departure &&
+            top5Departure.map((station: Top5Type) => (
+              <tr key={station.departureStationId}>
+                <td>{station.departureStationId}</td>
+                <td>{station.name1}</td>
+                <td>{station.returnStationId}</td>
+                <td>{station.name2}</td>
+                <td>{station.DepartureCount}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </>
   );
 };
@@ -89,6 +149,8 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
       stations: results[0],
       averageDepartureDistance: results[1],
       averageReturnDistance: results[2],
+      top5Departure: results[3],
+      top5Return: results[4],
     }, // will be passed to the page component as props
   };
 }
