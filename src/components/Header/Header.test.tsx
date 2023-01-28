5fa068ec388926dea69664ef8fb30a6e1df772d4
alari777 <alari777@gmail.com>
2023-01-28 13:48:37 +0200

    refactored snapshot test for `Header` component: was added `beforeEach()` with `Header` render

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,8 +2,11 @@ import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 describe('should render header', () => {
-  test('should render trips link in header navbar', () => {
+  beforeEach(() => {
     render(<Header />);
+  });
+
+  test('should render trips link in header navbar', () => {
     const navTrips = screen.getByTestId('nav-trips');
     expect(navTrips).toBeInTheDocument();
     expect(navTrips).toMatchSnapshot();
