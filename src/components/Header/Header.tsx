69ffabe8bc85263655fef7a9f80143193b89b905
alari777 <alari777@gmail.com>
2023-01-28 11:38:01 +0200

    added navigation panel with: `trips`, `stations` and `manage` pages

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,27 @@
 import { FC } from 'react';
 import styles from '@/components/Header/Header.module.scss';
+import Link from 'next/link';
 
 const Header: FC = () => {
-  return <header className={styles.header}>Helsinki city bike</header>;
+  return (
+    <header className={styles.header}>
+      <nav className='navbar navbar-expand-lg navbar-light'>
+        <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+          <div className='navbar-nav'>
+            <Link className='nav-item nav-link active' href='/'>
+              Trips
+            </Link>
+            <Link className='nav-item nav-link' href='/stations'>
+              Stations
+            </Link>
+            <Link className='nav-item nav-link' href='/manage'>
+              Manage: trips and stations
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
