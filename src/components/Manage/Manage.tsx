95f94a17dbfe7157bca08e6839272352966dc69b
alari777 <alari777@gmail.com>
2023-02-02 09:30:02 +0200

    added event onclick `truncateTables`

--- src/components/Manage/Manage.tsx
+++ src/components/Manage/Manage.tsx
@@ -10,9 +10,19 @@ interface ManageComponentProps {
 }
 
 const Manage: FC<ManageComponentProps> = ({ stations }) => {
+  const truncateTables = async ():Promise<void> => {
+    try {
+      const response = await fetch('/api/v1/truncate/all', {
+        method: 'DELETE',
+      });
+      if (response.status === 200) {
+        console.log('ok');
+      }
+    } catch (err) {}
+  }
   return (
     <>
-      <button>Clear tables Trips and Stations</button>
+      <button onClick={truncateTables}>Truncate tables Trips and Stations</button>
       <AddStationsRemotely />
       <AddStationsManually />
       <AddTripsRemotely />
