c6a99e77b44869036c431c4f7860997fd871e5ad
alari777 <alari777@gmail.com>
2023-02-02 10:28:11 +0200

    copied pixel of code from `Manage` component

--- src/components/Manage/TruncateTables/TruncateTables.tsx
+++ src/components/Manage/TruncateTables/TruncateTables.tsx
@@ -1,9 +1,48 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import Report from '@/components/Manage/Report/Report';
 
 const TruncateTables: FC = () => {
+  const [report, setReport] = useState<string[]>([]);
+  const [classSpinner, setClassSpinner] = useState<boolean>(false);
+
+  const truncateTables = async (): Promise<void> => {
+    setClassSpinner(true);
+    try {
+      setReport([]);
+      setReport((report) => [
+        ...report,
+        'Wait a little bit. Data is deleting ...',
+      ]);
+      const response = await fetch('/api/v1/truncate/all', {
+        method: 'DELETE',
+      });
+      if (response.status === 204) {
+        setReport((report) => [...report, 'Data was deleted.']);
+      } else {
+        setReport((report) => [...report, 'Tables are empty.']);
+      }
+    } catch (err) {
+      setReport((report) => [
+        ...report,
+        'Data was not deleted. Something went wrong. Please try again later.',
+      ]);
+    }
+    setClassSpinner(false);
+  };
+
   return (
     <>
-      <h1>Truncate Tables</h1>
+      <Report reports={report} typeReport='TruncateData' />
+      <button className='btn btn-primary mb-5' onClick={truncateTables}>
+        {classSpinner && (
+          <span
+            className='spinner-border spinner-border-sm'
+            role='status'
+            aria-hidden='true'
+          ></span>
+        )}
+        Truncate tables Trips and Stations
+      </button>
     </>
   );
 };
